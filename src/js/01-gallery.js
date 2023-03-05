cabfd1c4f73c6d1ee5aab3aa9f43ae4015195f1d
HrytsutenkoYaroslav <113903765+HrytsutenkoYaroslav@users.noreply.github.com>
2023-03-05 21:45:27 +0200

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainerUp = document.querySelector('.gallery');
+const cardsMarkup = createGalleryMark(galleryItems);
+
+galleryContainerUp.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+function createGalleryMark(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+</a>
+</div>
+`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
